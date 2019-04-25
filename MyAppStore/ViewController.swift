8702943db16564dcde871e9498a82f5c26d7390a
Jonathan Go <jogo88@gmail.com>
2019-04-25 11:27:41 +0800

    Initial Commit

---
+++ MyAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppStore
+//
+//  Created by Jonathan Go on 2019/04/25.
+//  Copyright © 2019 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
